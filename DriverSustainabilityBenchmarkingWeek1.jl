5a46ad0d00a8c74a09d2271d30b6162c9e91f5d1
Jakob Cetti Hansen <jkbch@coga.io>
2024-01-06 18:33:50 +0100

    jfslkdajf

--- DriverSustainabilityBenchmarkingWeek1.jl
+++ DriverSustainabilityBenchmarkingWeek1.jl
@@ -1,7 +1,11 @@
 using Plots
+using LinearAlgebra
+using SparseArrays
+
+include("DriverAMR17.jl")
 
 # Identify people behind the work
-studentID = "<INSERT YOUR STUDENTIDS HERE WITH COMMAS TO SEPARATE THEM>"
+studentID = "s214722, s214695, s204354"
 
 # Define input parameters (DO NOT CHANGE THIS PART)
 funu(x) = exp(-800*(x-0.4)^2) + 0.25*exp(-40*(x-0.8)^2)
@@ -17,26 +21,34 @@ maxit = 50
 # Let's call the FEM BVP 1D Solver with AMR
 # time the code using @time
 
+xAMR = []
+u = []
+iter = 0
+
 # Call Group <X> solver
 fac = 100  # we do multiple runs to get the average time
 total_time = 0.0
 for i in 1:fac
     t = @elapsed begin
-        xAMR, u, iter = DriverAMR17(L, c, d, x, func, tol, maxit)
+        global xAMR, u, iter = DriverAMR17(L, c, d, x, func, tol, maxit)
     end
-    total_time += t
+    global total_time += t
 end
 CPUtime = total_time / fac
+display(CPUtime)
 
 # Plot
-plot(xAMR, funu.(xAMR), linewidth=2, label="Exact")
-scatter!(xAMR, u, markersize=15, label="AMR", xlabel="x", ylabel="u", legend=:topleft)
+p = plot(xAMR, funu.(xAMR), linewidth=2, label="Exact", titlefontsize=7)
+scatter!(xAMR, u, markersize=2, label="AMR", xlabel="x", ylabel="u", legend=:topleft)
 
 DOF = length(xAMR)
 CO2eq = CPUtime / 3600 * 86 / 1000 * 0.135  # valid for macbook pro (assumed power consumption 105)
 
 title!(string("Group: $studentID, Iter: $iter, Time: ", round(CPUtime, digits=4), " s, DOF: $DOF, CO2e=", round(CO2eq, digits=4), " kg CO2"))
+display(p)
 
-# Element size distribution
-h = diff(xAMR)
-histogram(h, xlabel="h", ylabel="# of elements")
+if false
+    # Element size distribution
+    h = diff(xAMR)
+    display(histogram(h, xlabel="h", ylabel="# of elements"))
+end
